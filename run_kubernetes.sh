0a5da791740a3322256bb3d5f39f147c6e6c93b0
Mikisho <mikiye.82@gmail.com>
2020-05-01 13:07:35 -0500

    microservice-api-setup

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mickey24/udacityproj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udevopsproj4 --image=$dockerpath --port=8001
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udevopsproj4 8001:80
